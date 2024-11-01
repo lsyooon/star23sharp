87d15a67334c804642fc357672470cbd8cf7f811
hyun <a94460093@gmail.com>
2024-11-01 10:17:50 +0900

    feat: entity 생성

---
+++ star/src/main/java/com/ssafy/star/member/entity/Member.java
@@ -0,0 +1,43 @@
+package com.ssafy.star.member.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "member")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "member_name", nullable = false, unique = true)
+    private String memberName;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "complaint_count", nullable = false)
+    private int complaintCount = 0;
+
+    @Column(nullable = false)
+    private short state = 0;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(name = "reactivation_date")
+    private LocalDateTime reactivationDate;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+}
+
---
+++ star/src/main/java/com/ssafy/star/message/entity/Message.java
@@ -0,0 +1,79 @@
+package com.ssafy.star.message.entity;
+
+import com.ssafy.star.member.entity.Member;
+import com.ssafy.star.member.entity.MemberGroup;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Array;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "message")
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private Member sender;
+
+    @Column(name="receiver_type", nullable = false)
+    private int receiverType = 0;
+
+    @Column(name = "hint_image_first")
+    private String hintImageFirst;
+    @Column(name = "hint_image_second")
+    private String hintImageSecond;
+    @Column(name = "dot_hint_image")
+    private String dotHintImage;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Column
+    private String hint;
+
+    @Column
+    private float lat;
+    @Column
+    private float lng;
+    @Column
+    @JdbcTypeCode(SqlTypes.VECTOR)
+    @Array(length = 3)
+    private float[] coordinate;
+
+    @Column(name = "is_treasure", nullable = false)
+    private boolean isTreasure = false;
+
+    @Column(name = "is_found", nullable = false)
+    private boolean isFound = false;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column
+    private String image;
+
+    @Column
+    @JdbcTypeCode(SqlTypes.VECTOR)
+    @Array(length = 12288)
+    private float[] vector;
+
+    @ManyToOne
+    @JoinColumn(name = "group_id", foreignKey = @ForeignKey(name = "fk_message_group"))
+    private MemberGroup group;
+}
