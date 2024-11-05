f5f70368ee204eeff686c5d171e078799a897d3e
김민호 <oilpott12@naver.com>
2024-11-05 13:18:55 +0900

    feat: 지도 화면 구현
    - 지도 화면 구현
    - 위치 권한, 카메라 권한
    - 마커 리스트
    - 마커 상세
    - 사진 검증 후 검증 성공/실패 모달

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:kakao_map_plugin/kakao_map_plugin.dart';
 
 import 'package:provider/provider.dart';
 
@@ -9,7 +11,15 @@ import 'package:star23sharp/widgets/index.dart';
 //TODO - 카카오 연결
 //TODO - 카메라 연결
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: "assets/env/.env");
+
+  final appKey = dotenv.env['APP_KEY'] ?? '';
+  AuthRepository.initialize(
+    appKey: appKey,
+  );
+
   runApp(
     MultiProvider(
       providers: [
@@ -35,6 +45,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/home',
       routes: {
         '/home': (context) => const MainLayout(child: HomeScreen()),
+        '/map': (context) => const MainLayout(child: MapScreen()),
         // '/loading': (context) => const MainLayout(child: LoadingScreen()),
       },
     );
--- frontend/lib/screens/index.dart
+++ frontend/lib/screens/index.dart
@@ -1,2 +1,3 @@
 export 'package:star23sharp/screens/LoadingScreen.dart';
 export 'package:star23sharp/screens/HomeScreen.dart';
+export 'package:star23sharp/screens/map_screen.dart';
