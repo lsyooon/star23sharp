47d263f7e3a421fc418b6ed3a57f46a97a927174
목요빈 <dyqls1214@naver.com>
2024-11-06 04:22:05 +0900

    feat: 중복검사 및 회원가입 API 연결

--- frontend/lib/screens/user/signup_screen.dart
+++ frontend/lib/screens/user/signup_screen.dart
@@ -1,8 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:star23sharp/services/user_service.dart';
 import 'package:star23sharp/widgets/index.dart';
 
-class SignUpScreen extends StatelessWidget {
+class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
+
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final TextEditingController idController = TextEditingController();
+  final TextEditingController nicknameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController();
+
+  // 아이디 및 닉네임 중복 여부 상태 변수
+  bool isIdAvailable = false;
+  bool isNicknameAvailable = false;
+
+  Future<void> checkIdAvailability() async {
+    // 아이디 중복 검사 로직 구현
+    bool response = await UserService.checkDuplicateId(0, idController.text.trim());
+    setState(() {
+      isIdAvailable = !response;
+    });
+  }
+
+  Future<void> checkNicknameAvailability() async {
+    // 닉네임 중복 검사 로직 구현
+    bool response = await UserService.checkDuplicateId(1, nicknameController.text.trim());
+    setState(() {
+      isNicknameAvailable = !response;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +50,6 @@ class SignUpScreen extends StatelessWidget {
             ),
           ),
         ),
-
         Center(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 60),
@@ -30,9 +60,10 @@ class SignUpScreen extends StatelessWidget {
                   const Text(
                     "회원가입",
                     style: TextStyle(
-                        color: Colors.white,
-                        fontSize: FontSizes.title,
-                        fontWeight: FontWeight.bold),
+                      color: Colors.white,
+                      fontSize: FontSizes.title,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                   const SizedBox(height: 20),
                   _buildSignUpForm(context),
@@ -46,28 +77,6 @@ class SignUpScreen extends StatelessWidget {
   }
 
   Widget _buildSignUpForm(BuildContext context) {
-    final TextEditingController usernameController = TextEditingController();
-    final TextEditingController nicknameController = TextEditingController();
-    final TextEditingController passwordController = TextEditingController();
-    final TextEditingController confirmPasswordController =
-        TextEditingController();
-
-    // 아이디 및 닉네임 중복 여부를 저장할 변수
-    bool isUsernameAvailable = false;
-    bool isNicknameAvailable = false;
-
-    void checkIdAvailability() {
-      // 아이디 중복 검사 로직 구현
-      print("아이디 중복 검사 실행");
-      // isUsernameAvailable = true; // 예시
-    }
-
-    void checkNicknameAvailability() {
-      // 닉네임 중복 검사 로직 구현
-      print("닉네임 중복 검사 실행");
-      // isNicknameAvailable = true; // 예시
-    }
-
     return Container(
       height: MediaQuery.of(context).size.height * 0.4,
       padding: const EdgeInsets.all(20),
@@ -80,43 +89,43 @@ class SignUpScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            _buildTextFieldWithBtn("아이디", usernameController, isAvailable: () {
-              checkIdAvailability();
-            }),
-            _buildTextFieldWithBtn("닉네임", nicknameController, isAvailable: () {
-              checkNicknameAvailability();
-            }),
+            _buildTextFieldWithBtn("아이디", idController, isIdAvailable, checkIdAvailability),
+            _buildTextFieldWithBtn("닉네임", nicknameController, isNicknameAvailable, checkNicknameAvailability),
             _buildTextField("비밀번호", passwordController, obscureText: true),
-            _buildTextField("비밀번호 확인", confirmPasswordController,
-                obscureText: true),
+            _buildTextField("비밀번호 확인", confirmPasswordController, obscureText: true),
             const SizedBox(height: 20),
-            _buildSignUpButton(
-                context,
-                usernameController,
-                nicknameController,
-                passwordController,
-                confirmPasswordController,
-                isUsernameAvailable,
-                isNicknameAvailable),
+            _buildSignUpButton(context),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildTextFieldWithBtn(String label, TextEditingController controller,
-      {bool obscureText = false, required Function isAvailable}) {
+  Widget _buildTextFieldWithBtn(String label, TextEditingController controller, bool isAvailable, Function onPressed) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
           padding: const EdgeInsets.symmetric(vertical: 6),
-          child: Text(
-            label,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: FontSizes.label,
-            ),
+          child: Row(
+            children: [
+              Text(
+                label,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: FontSizes.label,
+                ),
+              ),
+              if (isAvailable) // 중복 검사 결과에 따라 체크 아이콘 표시
+                const Padding(
+                  padding: EdgeInsets.only(left: 8),
+                  child: Icon(
+                    Icons.check,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                ),
+            ],
           ),
         ),
         Row(
@@ -124,7 +133,6 @@ class SignUpScreen extends StatelessWidget {
             Expanded(
               child: TextField(
                 controller: controller,
-                obscureText: obscureText,
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: const Color(0xFFA292EC).withOpacity(0.4),
@@ -132,22 +140,16 @@ class SignUpScreen extends StatelessWidget {
                     borderRadius: BorderRadius.circular(10),
                     borderSide: BorderSide.none,
                   ),
-                  contentPadding: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                    vertical: 0,
-                  ),
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 0),
                 ),
               ),
             ),
-            const SizedBox(width: 10), // TextField와 버튼 사이의 간격
+            const SizedBox(width: 10),
             TextButton(
-              onPressed: () {
-                isAvailable(); // 중복 검사 함수 호출
-              },
+              onPressed: () => onPressed(), // 중복 검사 함수 호출
               style: TextButton.styleFrom(
                 backgroundColor: const Color(0xFFA292EC).withOpacity(0.4),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+                padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
                 ),
@@ -198,14 +200,7 @@ class SignUpScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildSignUpButton(
-      BuildContext context,
-      TextEditingController usernameController,
-      TextEditingController nicknameController,
-      TextEditingController passwordController,
-      TextEditingController confirmPasswordController,
-      bool isUsernameAvailable,
-      bool isNicknameAvailable) {
+  Widget _buildSignUpButton(BuildContext context) {
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -214,38 +209,48 @@ class SignUpScreen extends StatelessWidget {
           borderRadius: BorderRadius.circular(10),
         ),
         child: TextButton(
-          onPressed: () {
-            // 유효성 검사
-            String username = usernameController.text.trim();
+          onPressed: () async {
+            // 회원가입 처리 로직 추가
+            String memberId = idController.text.trim();
             String nickname = nicknameController.text.trim();
             String password = passwordController.text.trim();
             String confirmPassword = confirmPasswordController.text.trim();
 
-            if (username.isEmpty ||
+            if (memberId.isEmpty ||
                 nickname.isEmpty ||
                 password.isEmpty ||
                 confirmPassword.isEmpty) {
-              _showErrorDialog(context, '모든 필드를 입력해야 합니다.');
+              // _showErrorDialog(context, '모든 필드를 입력해야 합니다.');
+              print("필드입력");
               return;
             }
 
-            if (!isUsernameAvailable) {
-              _showErrorDialog(context, '아이디가 중복됩니다. 다른 아이디를 입력하세요.');
+            if (!isIdAvailable) {
+              // _showErrorDialog(context, '아이디가 중복됩니다. 다른 아이디를 입력하세요.');
+              print("아이디 입력");
               return;
             }
 
             if (!isNicknameAvailable) {
-              _showErrorDialog(context, '닉네임이 중복됩니다. 다른 닉네임을 입력하세요.');
+              // _showErrorDialog(context, '닉네임이 중복됩니다. 다른 닉네임을 입력하세요.');
+              print("니네임 입력");
               return;
             }
 
             if (password != confirmPassword) {
-              _showErrorDialog(context, '비밀번호가 일치하지 않습니다.');
+              // _showErrorDialog(context, '비밀번호가 일치하지 않습니다.');
+              print("비번 입력");
               return;
             }
 
-            // 회원가입 처리 로직 추가
-            // 예: API 호출
+            bool response = await UserService.signup(memberId, password, nickname);
+            if(response){
+              // 회원가입 완료
+              Navigator.pushNamed(context, '/home');
+            }else{
+              // 회원가입 실패
+              print("실패");
+            }
           },
           style: TextButton.styleFrom(
             padding: const EdgeInsets.symmetric(vertical: 14),
@@ -256,31 +261,6 @@ class SignUpScreen extends StatelessWidget {
           ),
         ),
       ),
-    );
-  }
-
-  void _showErrorDialog(BuildContext context, String message) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text(
-            '오류',
-            style: TextStyle(
-              fontSize: 20,
-            ),
-          ),
-          content: Text(message),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('확인'),
-            ),
-          ],
-        );
-      },
     );
   }
 }
