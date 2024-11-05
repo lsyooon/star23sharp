9e10ebec6628ce95ecf3a4f7eb77a487f82c4e6b
목요빈 <dyqls1214@naver.com>
2024-11-05 11:15:22 +0900

    feat: index 파일 추가

--- frontend/lib/widgets/index.dart
+++ frontend/lib/widgets/index.dart
@@ -1,3 +1,4 @@
 export "package:star23sharp/widgets/layouts/main_layout.dart";
 export "package:star23sharp/widgets/layouts/screen_size.dart";
 export "package:star23sharp/widgets/logo.dart";
+export "package:star23sharp/constant/font_size.dart";
